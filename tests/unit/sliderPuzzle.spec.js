d97084bc3bd38b820f4509fcef8212201f166709
NikitaLejnev <catassasin331@gmail.com>
2022-01-15 10:33:05 +0300

    Refactored sliderPuzzle.spec.js

--- tests/unit/sliderPuzzle.spec.js
+++ tests/unit/sliderPuzzle.spec.js
@@ -2,18 +2,23 @@ import { mount } from '@vue/test-utils';
 import SliderPuzzle from '@/components/SliderPuzzle.vue';
 import 'jest-localstorage-mock';
 
-jest.useFakeTimers();
+let wrapper;
 
-describe('SliderPuzzle.vue', () => {
+describe('Tests with simple setup', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    jest.spyOn(global, 'setInterval');
+    jest.spyOn(global, 'clearInterval');
+    wrapper = mount(SliderPuzzle);
+  });
+
   it('inserts the index of the image to swap when we click on an image', () => {
-    const wrapper = mount(SliderPuzzle);
     wrapper.find('#start-button').trigger('click');
     wrapper.find('img').trigger('click');
     expect(wrapper.vm.indexesToSwap.length).toBeGreaterThan(0);
   });
 
   it('swaps the image order when 2 images are clicked', () => {
-    const wrapper = mount(SliderPuzzle);
     wrapper.find('#start-button').trigger('click');
     const [firstImage, secondImage] = wrapper.vm.shuffledPuzzleArray;
     wrapper.get('.column:nth-child(1) img').trigger('click');
@@ -25,20 +30,36 @@ describe('SliderPuzzle.vue', () => {
   });
 
   it('starts timer when start method is called', () => {
-    const wrapper = mount(SliderPuzzle);
     wrapper.vm.start();
     expect(setInterval).toHaveBeenCalledTimes(1);
     expect(setInterval).toHaveBeenLastCalledWith(expect.any(Function), 1000);
   });
 
   it('stops timer when stop method is called', () => {
-    const wrapper = mount(SliderPuzzle);
     wrapper.vm.stop();
     expect(clearInterval).toHaveBeenCalledTimes(1);
   });
 
+  it('starts timer with Start button is clicked', () => {
+    wrapper.get('#start-button').trigger('click');
+    expect(setInterval).toHaveBeenCalledTimes(1);
+  });
+
+  it('stops timer with Quit button is clicked', () => {
+    wrapper.get('#quit-button').trigger('click');
+    expect(clearInterval).toHaveBeenCalledTimes(1);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+});
+
+describe('Tests with complicated setup', () => {
   it('records record to local storage', () => {
-    const wrapper = mount(SliderPuzzle, {
+    wrapper = mount(SliderPuzzle, {
       data() {
         return {
           currentDateTime: new Date(),
@@ -52,20 +73,8 @@ describe('SliderPuzzle.vue', () => {
     expect(localStorage.setItem).toHaveBeenCalledWith('records', stringifiedRecords);
   });
 
-  it('starts timer with Start button is clicked', () => {
-    const wrapper = mount(SliderPuzzle);
-    wrapper.get('#start-button').trigger('click');
-    expect(clearInterval).toHaveBeenCalledTimes(1);
-  });
-
-  it('stops timer with Quit button is clicked', () => {
-    const wrapper = mount(SliderPuzzle);
-    wrapper.get('#quit-button').trigger('click');
-    expect(clearInterval).toHaveBeenCalledTimes(1);
-  });
-
   it('shows the elapsed time', () => {
-    const wrapper = mount(SliderPuzzle, {
+    wrapper = mount(SliderPuzzle, {
       data() {
         return {
           currentDateTime: new Date(2020, 0, 1, 0, 0, 1),
