f969facf016752076a605f9b47a2a74554bb752b
NikitaLejnev <catassasin331@gmail.com>
2022-01-16 10:24:12 +0300

    Added jest.useFakeTimers() to sliderPuzzle.spec.js

--- tests/unit/sliderPuzzle.spec.js
+++ tests/unit/sliderPuzzle.spec.js
@@ -6,6 +6,7 @@ let wrapper;
 
 describe('Tests with simple setup', () => {
   beforeEach(() => {
+    jest.useFakeTimers();
     jest.spyOn(global, 'setInterval');
     jest.spyOn(global, 'clearInterval');
     wrapper = mount(SliderPuzzle);
